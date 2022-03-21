83b118517b39650263e40af1c1493bfce0c6683e
Inny Choi <innychoi93@gmail.com>
2022-03-21 15:47:50 -0500

    theme created

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -4,23 +4,46 @@ import PersonIcon from '@mui/icons-material/Person';
 import MapIcon from '@mui/icons-material/Map';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import Typography from '@mui/material/Typography';
 import { Outlet, Link } from "react-router-dom";
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 
 const App = () => {
     const [value, setValue] = React.useState("/");
     const handleChange = (event, newValue) => {
       setValue(newValue);
     };
+
+    const theme = createTheme({
+      typography: {
+        fontFamily: 'Roboto'
+      },
+    });
+
   return (
     <>
-      <h1>Pupper</h1>
-      <BottomNavigation value={value} onChange={handleChange} showLabels={true}>
-          <BottomNavigationAction label="swipe" value="/swipe" icon={<FavoriteIcon />}  component={Link} to='/'/>
-          <BottomNavigationAction label="map" value="/" icon={<MapIcon />} component={Link} to='/map'/>
-          <BottomNavigationAction label="profile" value="/profile" icon={<PersonIcon /> } component={Link} to='/profile'/>
-          <BottomNavigationAction label="matches" value="/matches" icon={<ChatBubbleIcon />} component={Link} to='/matches'/>
+      <ThemeProvider theme={theme}>
+      <Typography style={{ fontSize: 30, fontWeight: 700, color: '#ff9800', textAlign: 'center', fontFamily:'Courgette' }}>Pupper</Typography>
+      <Outlet />
+      <BottomNavigation value={value} onChange={handleChange} showLabels={true}  sx={{
+          bgcolor: '#ff9800',
+          '& .Mui-selected': {
+            '& .MuiBottomNavigationAction-label': {
+              fontSize: theme => theme.typography.caption,
+              transition: 'none',
+              lineHeight: '20px'
+            },
+            '& .MuiSvgIcon-root, & .MuiBottomNavigationAction-label': {
+              color: "#bf360c"
+            }
+          }
+        }}>
+          <BottomNavigationAction sx={{color: "#fff"}} label="swipe" value="/swipe" icon={<FavoriteIcon />}  component={Link} to='/swipe'/>
+          <BottomNavigationAction sx={{color: "#fff"}} label="map" value="/map" icon={<MapIcon />} component={Link} to='/map'/>
+          <BottomNavigationAction sx={{color: "#fff"}} label="matches" value="/matches" icon={<ChatBubbleIcon />} component={Link} to='/matches'/>
+          <BottomNavigationAction sx={{color: "#fff"}} label="profile" value="/profile" icon={<PersonIcon /> } component={Link} to='/profile'/>
       </BottomNavigation>
-     <Outlet />
+      </ThemeProvider>
     </>
   )
 };
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 // const controller = require('./Controller')
@@ -12,6 +13,14 @@ app.use(express.static('client/dist'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`)
