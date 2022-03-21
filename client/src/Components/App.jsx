40537be0f35201dab849114c3da91382cd7d3fb7
Inny Choi <innychoi93@gmail.com>
2022-03-21 13:55:02 -0500

    material ui navbar working

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -7,7 +7,6 @@ import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
 import { Outlet, Link } from "react-router-dom";
 
 const App = () => {
-  const pathname = window.location.pathname; // in case user visits the path directly. The BottomNavBar is able to follow suit.
     const [value, setValue] = React.useState("/");
     const handleChange = (event, newValue) => {
       setValue(newValue);
@@ -15,12 +14,11 @@ const App = () => {
   return (
     <>
       <h1>Pupper</h1>
-
       <BottomNavigation value={value} onChange={handleChange} showLabels={true}>
-          <BottomNavigationAction label="map" value="/" icon={<MapIcon />} component={Link} to='/'/>
+          <BottomNavigationAction label="swipe" value="/swipe" icon={<FavoriteIcon />}  component={Link} to='/'/>
+          <BottomNavigationAction label="map" value="/" icon={<MapIcon />} component={Link} to='/map'/>
           <BottomNavigationAction label="profile" value="/profile" icon={<PersonIcon /> } component={Link} to='/profile'/>
-          <BottomNavigationAction label="swipe" value="/swipe" icon={<FavoriteIcon />}  component={Link} to='/swipe'/>
-          <BottomNavigationAction label="chat" value="/chat" icon={<ChatBubbleIcon />} component={Link} to='/chat'/>
+          <BottomNavigationAction label="matches" value="/matches" icon={<ChatBubbleIcon />} component={Link} to='/matches'/>
       </BottomNavigation>
      <Outlet />
     </>
@@ -28,25 +26,3 @@ const App = () => {
 };
 
 export default App;
-
-{/* <Link to="/profile">Profile</Link>
-<Link to="/swipe">Swipe</Link>
-<Link to="/map">Map</Link>
-<Link to="/matches">Matches</Link> */}
-{/* <BottomNavigationAction label="Swipe" icon={<FavoriteIcon />} to={"/swipe"} component={Link}/>
-<BottomNavigationAction label="Map" icon={<MapIcon />} to={"/map"} component={Link}/>
-<BottomNavigationAction label="Matches" icon={<ChatBubbleIcon />} to={"/matches"} component={Link}/> */}
-
-{/* <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      ></BottomNavigation> */}
-
-
-      {/* <BottomNavigationAction label="Profile" icon={<PersonIcon />} />
-      <BottomNavigationAction label="Swipe" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Map" icon={<MapIcon />} />
-      <BottomNavigationAction label="Matches" icon={<ChatBubbleIcon />} /> */}
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import App from './Components/App.jsx';
 import Map from './Components/Map.jsx';
 import Profile from './Components/Profile.jsx';
@@ -10,13 +14,15 @@ import Matches from './Components/Matches.jsx';
 const appElement = document.getElementById('app');
 
 ReactDOM.render(
-  <Router>
-
-        <Route exact path="/" component={Map} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/swipe" component={Swipe} />
-        <Route path="/chat" component={Matches} />
-
-  </Router>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="profile" element={<Profile />} />
+        <Route path="map" element={<Map />} />
+        <Route path="swipe" element={<Swipe />} />
+        <Route path="matches" element={<Matches />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  appElement
 );
-
