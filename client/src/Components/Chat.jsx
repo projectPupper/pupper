4ae624c8b8fc0b12ac474aae1e5def2adf113aa2
Anthony Blelloch <anthony.be510@gmail.com>
2022-03-22 12:23:10 -0700

    create chat modal

--- client/src/Components/Chat.jsx
+++ client/src/Components/Chat.jsx
@@ -4,11 +4,9 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import ReactDom from 'react-dom';
 
-const Chat = ( { closeChat }) => {
-  // display match profile picture and name at the top of the chat
-  // find out if current match and user have a chat
-  // render out chat (if none exists, will be empty)
-  // include an input field for new message
+const Chat = ({ closeChat }) => {
+  const [input, setInput] = useState(null);
+  // const [send, setSend] = useState('disabled');
   const [chats, setChats] = useState({
     id: '12',
     time: '54321',
@@ -21,26 +19,56 @@ const Chat = ( { closeChat }) => {
       body: 'test 2',
       sender: '67891'
     }]
-    }
+  }
   )
+  const handleChange = (e) => {
+    e.preventDefault();
+    // setSend('');
+    let message = {
+      body: e.target.value,
+      sender: '12345'
+    }
+    setInput(message);
+  }
 
-  return ReactDom.createPortal(
-    <div style={{zindex: '10001', backgroundColor: 'purple'}} >
-    <div style={{zindex: '1000', position: 'absolute', backgroundColor: 'white'}} >
-       {
+  const sendMessage = (e) => {
+    e.preventDefault();
+    // setSend('disabled');
+    let messages = chats;
+    messages.messages.push(input);
+    setChats(messages);
+  }
+
+  return (
+    <div>
+      {
         chats.messages.map((message, index) => {
           return <Messages message={message} key={index} />
         })
-       }
-        <TextField id="filled-basic" label="Type a Message" variant="filled" />
-        <Button disabled>Send</Button>
-        <Button onClick={closeChat}>Close</Button>
+      }
+      <TextField id="filled-basic" label="Type a Message" variant="filled" onChange={handleChange} />
+      <Button onClick={sendMessage}>Send</Button>
     </div>
-    </div>,
-    document.getElementById('chatPortal'),
   )
 }
 
 export default Chat;
 
+// <div style={{zindex: '10001', backgroundColor: 'purple'}} >
+// <div style={{zindex: '1000', position: 'absolute', backgroundColor: 'white'}} >
+//    {
+//     chats.messages.map((message, index) => {
+//       return <Messages message={message} key={index} />
+//     })
+//    }
+//     <TextField id="filled-basic" label="Type a Message" variant="filled" />
+//     <Button disabled>Send</Button>
+//     <Button onClick={closeChat}>Close</Button>
+// </div>
+// </div>,
+// document.getElementById('chatPortal'),
 
+  // display match profile picture and name at the top of the chat
+  // find out if current match and user have a chat
+  // render out chat (if none exists, will be empty)
+  // include an input field for new message
