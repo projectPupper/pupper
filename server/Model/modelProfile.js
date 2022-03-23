7ff59044ca3b78b9f07191f8a2629380f08f5d13
Austin Wang <austinthewang@gmail.com>
2022-03-23 15:15:57 -0700

    update

--- server/Model/modelProfile.js
+++ server/Model/modelProfile.js
@@ -2,24 +2,25 @@ const { Profile } = require('../../db/db.js')
 
 module.exports = {
   findProfile: (uid, _id, cb) => {
-    // console.log('uid model', uid)
-    // //, _id
-    // let query;
-    // if (uid) {
-    //   query = Profile.find({uid})
-    //   console.log(typeof query)
-    // }
-    // if (_id) {
-    //   query = Profile.find({_id})
-    // }
-    // query()
-    Profile.find({uid})
-    .then((result) => {
-      cb(null, result[0])
-    })
-    .catch((err) => {
-      cb(err);
-    })
+    if (uid) {
+      Profile.find({uid})
+        .then((result) => {
+          cb(null, result[0])
+        })
+        .catch((err) => {
+          cb(err);
+        })
+    }
+    if (_id) {
+      Profile.find({_id})
+      .then((result) => {
+        cb(null, result[0])
+      })
+      .catch((err) => {
+        cb(err);
+      })
+    }
+
   },
 
   createProfile: (body, cb) => {
