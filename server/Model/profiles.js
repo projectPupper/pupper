ffb37fe75518dde38ac0585b10b95351b149d77b
Lorene Chew <lorene.chew@gmail.com>
2022-03-23 12:33:16 -0700

    useContext updated

--- server/Model/profiles.js
+++ server/Model/profiles.js
@@ -6,13 +6,13 @@ module.exports = {
     // db.Swipe.updateOne({_id: body.id}, )
   },
   getOtherProfiles: (query, cb) => {
-    db.Profile.find({_id: query._id})
+    db.Profile.find({_id: "623b6e8cd26a96dbf1b88ba0"})
     .then((res) => {
       console.log(res[0].swiped);
       let swipedList = res[0].swiped;
-      swipedList.push(query._id);
-      // db.Profile.find({ _id: {$nin: swipedList} })
-      db.Profile.find({})
+      swipedList.push("623b6e8cd26a96dbf1b88ba0");
+      db.Profile.find({ _id: {$nin: swipedList} })
+      // db.Profile.find({})
       .then((result) => {
         cb(null, result);
       })
