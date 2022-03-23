d691cc84f8b41dafa8be4882740ff495a4ae5d58
kbakalova <bakalovaa@gmail.com>
2022-03-23 16:13:15 -0700

    commit

--- client/src/Components/Filters.jsx
+++ client/src/Components/Filters.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Box from '@mui/material/Box';
@@ -11,6 +12,7 @@ import { borders } from '@mui/system';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { orange } from '@mui/material/colors';
+import breedList from '../../../breeds.js';
 
 
 
@@ -18,18 +20,33 @@ import { orange } from '@mui/material/colors';
 const Filters = () => {
 
   const [sizes, setSizes] = React.useState(() => []);
+  const [preferences, setPreferences] = React.useState(() => {});
 
   const handleSizes = (event, newSizes) => {
     setSizes(newSizes);
+    console.log(event);
   };
+
+  const handlePreferences = (event, preference) => {
+    setPreferences(event.target.value = preference);
+    console.log(preferences);
+  }
+
+
+  // const handleEnergy = (event, newEnergy) => {
+  //   setEnergy(newEnergy);
+  // };
 
   const theme = createTheme({
     palette: {
       primary: {
         light: orange[200],
-        main: "##ff9800",
+        main: orange[500],
         dark: orange[800],
       },
+    },
+    typography: {
+      fontFamily: 'Roboto'
     },
   });
 
@@ -53,10 +70,12 @@ const Filters = () => {
           m: 1,
         },
       }}>
+
+<Typography style={{ fontSize: 16, fontWeight: 500, color: '#ff9800', textAlign: 'left', fontFamily:'Courgette' }}>What Kind of Playmate is Your Pup Looking For?</Typography>
       <Autocomplete
         disablePortal
         id="combo-box-demo"
-        options={breeds}
+        options={breedList}
         sx={{ width: 300 }}
         renderInput={(params) => <TextField {...params} label="Breed" />}
       />
@@ -66,24 +85,28 @@ const Filters = () => {
           flexDirection: 'column',
           alignItems: 'center',
           '& > *': {
-            m: 1,
+             m: 0,
           },
         }}
       >
         <ThemeProvider theme={theme}>
+        <Typography style={{ fontSize: 14, fontWeight: 400, color: '#ff9800', textAlign: 'left', fontFamily:'Roboto' }}>Size</Typography>
           <ToggleButtonGroup fullWidth size="medium" aria-label="medium button group" value={sizes}
             onChange={handleSizes}>
+            <ToggleButton value="extraSmall" aria-label="extraSmall">Extra Small</ToggleButton>
             <ToggleButton value="small" aria-label="small">Small</ToggleButton>
             <ToggleButton value="medium">Medium</ToggleButton>
             <ToggleButton value="large">Large</ToggleButton>
           </ToggleButtonGroup>
 
+          <Typography style={{ fontSize: 14, fontWeight: 400, color: '#ff9800', textAlign: 'left', fontFamily:'Roboto' }}>Gender</Typography>
           <ToggleButtonGroup fullWidth size="medium" aria-label="medium button group" value={sizes}
-            onChange={handleSizes} >
+            onChange={handleSizes, handlePreferences}>
             <ToggleButton value="male" aria-label="male">Male</ToggleButton>
             <ToggleButton value="female" aria-label="female">Female</ToggleButton>
           </ToggleButtonGroup>
 
+          <Typography style={{ fontSize: 14, fontWeight: 400, color: '#ff9800', textAlign: 'left', fontFamily:'Roboto' }}>Age</Typography>
           <ToggleButtonGroup fullWidth size="medium" aria-label="medium button group" value={sizes}
             onChange={handleSizes} >
             <ToggleButton value="puppy" aria-label="puppy" >Puppy</ToggleButton>
@@ -91,20 +114,23 @@ const Filters = () => {
             <ToggleButton value="senior" aria-label="senior">Senior</ToggleButton>
           </ToggleButtonGroup>
 
+          <Typography style={{ fontSize: 14, fontWeight: 400, color: '#ff9800', textAlign: 'left', fontFamily:'Roboto' }}>Energy Level</Typography>
           <ToggleButtonGroup fullWidth size="medium" aria-label="medium button group" value={sizes}
             onChange={handleSizes} >
             <ToggleButton value="low" aria-label="low" >Low</ToggleButton>
-            <ToggleButton value="medium" aria-label="medium">Medium</ToggleButton>
+            <ToggleButton value="Medium" aria-label="medium">Medium</ToggleButton>
             <ToggleButton value="high" aria-label="high">High</ToggleButton>
           </ToggleButtonGroup>
 
           <ToggleButtonGroup fullWidth size="medium" aria-label="medium button group" value={sizes}
             onChange={handleSizes} >
-            <ToggleButton value="yes" aria-label="yes" >Yes</ToggleButton>
-            <ToggleButton value="no" aria-label="no">No</ToggleButton>
+            <ToggleButton value="yes" aria-label="yes" >On-Leash</ToggleButton>
+            <ToggleButton value="no" aria-label="no">Off-Leash</ToggleButton>
           </ToggleButtonGroup>
         </ThemeProvider>
-
+        <ThemeProvider theme={theme}>
+          <Button variant="contained" sx={{bgcolor: '#ff9800', '&:hover': {bgcolor: '#ff9800'}}}>Submit</Button>
+        </ThemeProvider>
       </Box>
     </Box>
 
