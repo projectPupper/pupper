258782ad63b31233fb5926f03a2fbfa1fcf44012
Lorene Chew <lorene.chew@gmail.com>
2022-03-24 11:34:13 -0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  REACT_APP_GOOGLE_MAPS_API_KEY: "AIzaSyAjdy0uLt2_Ajynp6TpfhGZh0ym1C4v-eU",
-  YELP_API_KEY: "BwDL8KP6X9hDs-HgPPLomyUfctPLyAUyBcIhfXcowvAABUJMhlgJQeiMchnp7Q4gOmX3JC8hv0Oij_xp-es7q0ei0qpI_YDq-MJUWAPL9JVfPltLIhvvkB3OMf84YnYx"
-}
